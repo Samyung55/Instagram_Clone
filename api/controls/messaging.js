3988ec7fa5e88d162d465d67b444c785fe3e265e
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-09-20 19:54:19 +0100

    Update messaging.js

--- api/controls/messaging.js
+++ api/controls/messaging.js
@@ -3,3 +3,22 @@ const Message = require("../models/message");
 const Chat = require("../models/cha");
 
 
+exports.newMessage = catchAsync(async (req, res, next) => {
+    const {chatId, content } = req.body;
+
+    const msgData = {
+        sender: req.user._id,
+        chatId,
+        content,
+    }
+
+    const newMessage = await Message.create(msgData);
+
+    await Chat.findByIdAndUpdate(chatId, { latestMessage: newMessage });
+
+    res.status(200).json({
+        successL true,
+        newMessage,
+    });
+});
+
