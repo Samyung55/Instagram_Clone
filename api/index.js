fec1c070e4643f4321d9e425fa131a7f6bfab727
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-09-29 00:39:47 +0530

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const errorMiddleware = require('./middlewares/error');
+const path = require('path');
+
+const app = express();
+
+
