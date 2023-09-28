defe712310993b4e5262fea834d2ce2090ea006b
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-09-29 00:42:10 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -3,3 +3,8 @@ const connectDatabase = require('./config/database');
 const PORT = process.env.PORT || 4000;
 
 connectDatabase();
+
+const server = app.listen(PORT, () => {
+    console.log(`Server Running on http://localhost:${PORT}`);
+});
+
