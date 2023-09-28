e9de7528f0a2acca6751855c2ba70ee755c9d18a
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-09-29 00:40:10 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,3 +6,7 @@ const path = require('path');
 const app = express();
 
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use('/public', express.static('public'));
