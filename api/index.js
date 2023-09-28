078615f873812befd2ee5a03634f09ef01a9e3b2
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-09-29 00:40:59 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -26,3 +26,17 @@ app.use('/api/v1', post);
 app.use('/api/v1', user);
 app.use('/api/v1', chat);
 app.use('/api/v1', message);
+
+// deployment
+__dirname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    });
+} else {
+    app.get('/', (req, res) => {
+        res.send('Server is Running! 🚀');
+    });
+}
