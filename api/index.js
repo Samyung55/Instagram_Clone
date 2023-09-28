187a9543b6fe2a09ef615891c77d9b651e0adf04
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-09-29 00:40:25 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -10,3 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/public', express.static('public'));
+
+if (process.env.NODE_ENV != "production") {
+    require('dotenv').config({ path: 'backend/config/config.env' });
+}
